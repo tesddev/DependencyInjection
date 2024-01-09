3c7cad2264dbb42a1fa75ec8eae9bb01650a9a09
tesddev <tesleem.amuda@gmail.com>
2024-01-09 13:44:50 +0100

    Initial Commit

---
+++ DependencyInjection/DependencyInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionApp.swift
+//  DependencyInjection
+//
+//  Created by GIGL-MAC on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
